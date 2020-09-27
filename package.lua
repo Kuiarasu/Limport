058c4ca75c8674a277796074fd1fe5b40d436b54
Ari <phantomcrazyheart@gmail.com>
2020-09-27 11:33:52 -0400

    Version 0.0.3
    - Updated tags to match correct tags

--- package.lua
+++ package.lua
@@ -2,7 +2,7 @@ return {
     name = 'Kuiarasu/Limport';
 	version = '0.0.2';
 	description = 'A small library that allows you to import files or directories as libraries regardless of the default require supporting file and/or directory libraries.';
-	tags = {'luvit', 'luvi', 'server', 'env'};
+	tags = {'luvit', 'luvi', 'env', 'module'};
 	license = 'MIT';
 	author = {
 		name = 'Ari';
